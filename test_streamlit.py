cb5f0b5a43bca82b53184a3346a5e1e5dfecaa96
abluna <baldenegro40@gmail.com>
2024-06-15 16:10:43 -0700

    Create test_streamlit.py
    adding app code

---
+++ test_streamlit.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+from PIL import Image
+
+
+st.write("""
+# Dog Classification Tool
+Testing this app
+""")
+
+#########################
+## Importing the image ##
+#########################
+
+img = st.file_uploader("Upload the image", type=None)
+
+left_co,cent_co,last_co = st.columns(3)
+with cent_co:   
+    if img is not None:
+        original_image = Image.open(img)
+        st.image(original_image, caption='Your Image', width = 250)
+
+#########################
+## Exporting the image ##
+#########################
+
+left_co,cent_co,last_co = st.columns(3)
+with cent_co:
+    if img is not None:
+        if st.button("Rotate Image", type="primary"):
+            original_image = Image.open(img)
+            rotated_image = original_image.rotate(180)
+            st.image(rotated_image, caption='Rotated Image', width = 250)
+    
